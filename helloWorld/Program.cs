95809288976a11c2eb10b2618c90193ff6ac9b98
jafarimahdi <mahdijafarii.mj@gmail.com>
2022-06-07 22:41:09 +0200

    hello C#

---
+++ helloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
+
