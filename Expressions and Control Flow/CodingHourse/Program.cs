895ae67c8baf991adda290fc1e32fed72bb1128d
jafarimahdi <mahdijafarii.mj@gmail.com>
2022-08-22 19:06:11 +0200

    basic of C#

---
+++ Expressions and Control Flow/CodingHourse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CodingHourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // An average Green Fox attendee codes 6 hours daily
+            // The semester is 17 weeks long
+            int dailyCodingHours = 6;
+            int semesterWeeks = 17;
+
+
+            // Print how many hours is spent with coding in a semester by an attendee,
+            // if the attendee only codes on workdays.
+            int weeklyCodingHourses = 5;
+            int codingHourse = (dailyCodingHours * weeklyCodingHourses) * semesterWeeks;
+
+            Console.WriteLine(codingHourse);
+
+            // Print the percentage of the coding hours in the semester if the average
+            // work hours weekly is 52
+
+            int weeklyHourse = 52;
+
+            double avregeInPercent = (100 / weeklyHourse) * (dailyCodingHours * weeklyCodingHourses);
+
+
+            Console.WriteLine(avregeInPercent + "%");
+        }
+    }
+}
+
---
+++ Expressions and Control Flow/TwoNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a program that prints a few operations on two numbers: 22 and 13
+            int a = 13;
+            int b = 22;
+            // Print the result of 13 added to 22
+            Console.WriteLine(a + b);
+
+            // Print the result of 13 substracted from 22
+            Console.WriteLine(a - b);
+
+            // Print the result of 22 multiplied by 13
+            Console.WriteLine(a * b);
+
+            // Print the result of 22 divided by 13 (as a decimal fraction)
+            Console.WriteLine(a  / b);
+
+            // Print the integer part of 22 divided by 13
+            Console.WriteLine(b / a);
+
+            // Print the remainder of 22 divided by 13
+            Console.WriteLine(a % b);
+        }
+    }
+}
+
