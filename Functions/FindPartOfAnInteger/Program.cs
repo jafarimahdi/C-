98897407213a189f8597b1e3417a0f59d460961f
jafarimahdi <mahdijafarii.mj@gmail.com>
2022-09-01 08:00:48 +0200

    Basic of C#_Functions & Method

---
+++ Functions/FindPartOfAnInteger/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace FindPartOfAnInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Create a function that takes a number and an array of integers as parameters
+            //  It returns the indices of the integers of the array which contain the given number
+            //  or returns an empty array (if the number is not part of any of the integers in the array)
+
+            //  Example:
+            Console.WriteLine($"[{FindMatchingIndexes(1, new int[] { 1, 11, 34, 52, 61 })}]");
+            //  should print: `[0, 1, 4]`
+            Console.WriteLine($"[{FindMatchingIndexes(9, new int[] { 1, 11, 34, 52, 61 })}]");
+            //  should print: '[]'
+
+        }
+
+        public static string FindMatchingIndexes(int num, int[] userArray)
+        {
+            string indexOfElement = "";
+
+
+            for (int i = 0; i < userArray.Length; i++)
+            {
+                if (userArray[i].ToString().Contains(num.ToString()))
+                {
+                    indexOfElement += i + ",";
+                }
+            }
+            return indexOfElement;
+        }
+
+        // Due to array protocol, we have to find the array size in the begining, and in this situation we are not able to it.
+        // that's we change the format to string to get the resutl and more capacity 
+    }
+}
+
--- WeekendPractice/Array/Program.cs
+++ WeekendPractice/Array/Program.cs
@@ -9,159 +9,159 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            // **** Basic Array
+            // ***************************************** Basic Array
 
-            //int[] normalArray = new int[4];
-            //normalArray[0] = 1;
-            //normalArray[1] = 5;
-            //normalArray[2] = 7;
-            //normalArray[3] = 8;
+            int[] normalArray = new int[4];
+            normalArray[0] = 1;
+            normalArray[1] = 5;
+            normalArray[2] = 7;
+            normalArray[3] = 8;
 
-            //for (int i = 0; i < normalArray.Length; i++)
-            //{
-            //    Console.WriteLine(normalArray[i]);
-            //};
+            for (int i = 0; i < normalArray.Length; i++)
+            {
+                Console.WriteLine(normalArray[i]);
+            };
 
-            //Console.WriteLine("---------------------------");
-
-
-            //// ****** defind in direct way 
-
-            //int[] normalArray2 = { 2, 4, 5, 6 };
-
-            //for (int i = 0; i < normalArray.Length; i++)
-            //{
-            //    Console.WriteLine(normalArray2[i]);
-            //};
+            Console.WriteLine("---------------------------");
 
 
+            // ******************************* defind in direct way 
 
-            // ***  Multi Dimensional Array    1- Basic and old
+            int[] normalArray2 = { 2, 4, 5, 6 };
 
-            //int[,] multiDimen = new int[2, 2];
-            //multiDimen[0, 0] = 13;
-            //multiDimen[0, 1] = 9;
-            //multiDimen[1, 0] = 6;
-            //multiDimen[1, 1] = 9;
-
-            //for (int i = 0; i < 2; i++)
-            //{
-            //    for (int j = 0; j < 2; j++)
-            //    {
-            //        Console.Write(multiDimen[i, j] + " ");
-            //    }
-
-            //    Console.WriteLine();  //******* jump to the next line
-            //}
-
-            //Console.WriteLine("---------------------------");
-
-            // 2- second way of finding the multidim
-
-            //int[,] multiDimen2 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
-
-            //for (int i = 0; i < 2; i++)
-            //{
-            //    for (int j = 0; j < 3; j++)
-            //    {
-            //        Console.Write(multiDimen2[i, j] + " ");
-
-            //    }
-            //    Console.WriteLine();
-            //}
+            for (int i = 0; i < normalArray.Length; i++)
+            {
+                Console.WriteLine(normalArray2[i]);
+            };
 
 
-            //Console.WriteLine("---------------------------");
+
+            // *****************************  Multi Dimensional Array    1- Basic and old
+
+            int[,] multiDimen = new int[2, 2];
+            multiDimen[0, 0] = 13;
+            multiDimen[0, 1] = 9;
+            multiDimen[1, 0] = 6;
+            multiDimen[1, 1] = 9;
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(multiDimen[i, j] + " ");
+                }
+
+                Console.WriteLine();  //******* jump to the next line
+            }
+
+            Console.WriteLine("---------------------------");
+
+            // 2- *****************************   second way of finding the multidim
+
+            int[,] multiDimen2 = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(multiDimen2[i, j] + " ");
+
+                }
+                Console.WriteLine();
+            }
+
+
+            Console.WriteLine("---------------------------");
 
 
             // Define in direct way  -Collection Initializer         3- third way of definding multi array 
 
-            //int[,] multiDimen3 = { { 1, 2, 3 }, { 4, 5, 6 } };
+            int[,] multiDimen3 = { { 1, 2, 3 }, { 4, 5, 6 } };
 
-            //for (int i = 0; i < multiDimen2.GetLength(0); i++)
-            //{
-            //    for (int j = 0; j < multiDimen2.GetLength(1); j++)
-            //    {
-            //        Console.Write(multiDimen3[i, j] + " ");
+            for (int i = 0; i < multiDimen2.GetLength(0); i++)
+            {
+                for (int j = 0; j < multiDimen2.GetLength(1); j++)
+                {
+                    Console.Write(multiDimen3[i, j] + " ");
 
-            //    }
-            //    Console.WriteLine();
+                }
+                Console.WriteLine();
 
-            //}
+            }
 
 
             // ******** Jagged Array
 
             // 1- old way 
 
-            //int[][] jaggedArray = new int[2][];
-            //jaggedArray[0] = new int[2];
-            //jaggedArray[1] = new int[4];
+            int[][] jaggedArray = new int[2][];
+            jaggedArray[0] = new int[2];
+            jaggedArray[1] = new int[4];
 
 
-            //jaggedArray[0][0] = 8;
-            //jaggedArray[0][1] = 3;
+            jaggedArray[0][0] = 8;
+            jaggedArray[0][1] = 3;
 
-            //jaggedArray[1][0] = 5;
-            //jaggedArray[1][1] = 9;
-            //jaggedArray[1][3] = 0;
-            //jaggedArray[1][3] = 83;
+            jaggedArray[1][0] = 5;
+            jaggedArray[1][1] = 9;
+            jaggedArray[1][3] = 0;
+            jaggedArray[1][3] = 83;
 
-            //for (int i = 0; i < 2; i++)
-            //{
-            //    for (int j = 0; j < jaggedArray[i].Length; j++)
-            //    {
-            //        Console.Write(jaggedArray[i][j] + " ");
-            //    }
-            //    Console.WriteLine();
-            //}
+            for (int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < jaggedArray[i].Length; j++)
+                {
+                    Console.Write(jaggedArray[i][j] + " ");
+                }
+                Console.WriteLine();
+            }
 
 
-            //Console.WriteLine("---------------------------");
+            Console.WriteLine("---------------------------");
             //// second way of definition Collection intionalion 
 
-            //int[][] jaggedArray2 = new int[3][];
-            //jaggedArray2[0] = new int[] { 3, 5, 8, 9, 3 };
-            //jaggedArray2[1] = new int[] { 1, 4, 2, 5, 6, 8, 9, 0, 4 };
-            //jaggedArray2[2] = new int[] { 4, 6, 8, 9, 0, 3, 2, 4, 5 };
+            int[][] jaggedArray2 = new int[3][];
+            jaggedArray2[0] = new int[] { 3, 5, 8, 9, 3 };
+            jaggedArray2[1] = new int[] { 1, 4, 2, 5, 6, 8, 9, 0, 4 };
+            jaggedArray2[2] = new int[] { 4, 6, 8, 9, 0, 3, 2, 4, 5 };
 
-            //for (int i = 0; i < jaggedArray2.Length; i++)
-            //{
-            //    foreach (int item in jaggedArray2[i])
-            //    //for (int j = 0; j < jaggedArray2[i].Length; j++)
-            //    {
-            //        Console.Write(item + " ");
-            //        //Console.Write(jaggedArray2[i][j] + " ");
+            for (int i = 0; i < jaggedArray2.Length; i++)
+            {
+                foreach (int item in jaggedArray2[i])
+                //for (int j = 0; j < jaggedArray2[i].Length; j++)
+                {
+                    Console.Write(item + " ");
+                    //Console.Write(jaggedArray2[i][j] + " ");
 
-            //    }
-            //    Console.WriteLine();
-            //}
+                }
+                Console.WriteLine();
+            }
 
-            //Console.WriteLine("---------------------------");
+            Console.WriteLine("---------------------------");
 
 
 
-            //int[][] jaggedArray3 = new int[][]
-            //{
-            //new int[] { 9, 3, 3, 5, 8 },
-            //new int[] { 8, 9, 0, 4, 1, 4, 2, 5, 6 },
-            //new int[] { 0, 3, 2, 4 }
-            //};
+            int[][] jaggedArray3 = new int[][]
+            {
+            new int[] { 9, 3, 3, 5, 8 },
+            new int[] { 8, 9, 0, 4, 1, 4, 2, 5, 6 },
+            new int[] { 0, 3, 2, 4 }
+            };
 
-            //for (int i = 0; i < jaggedArray3.Length; i++)
-            //{
-            //    foreach (int item in jaggedArray3[i])
-            //    {
-            //        Console.Write(item + " ");
-            //    }
-            //    Console.WriteLine();
-            //}
+            for (int i = 0; i < jaggedArray3.Length; i++)
+            {
+                foreach (int item in jaggedArray3[i])
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
 
 
 
 
             //  Collection     1- ArrayList
-                      
+
 
 
             ArrayList hashmamad = new ArrayList(); // Array in Javascript 
