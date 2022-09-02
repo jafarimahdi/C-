1520211cad588cfcb236041c883678bb39c83359
jafarimahdi <mahdijafarii.mj@gmail.com>
2022-09-02 21:20:53 +0200

    C# basic of function : Palindrome, Bubble

---
+++ Functions/Bubble/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Bubble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Create a function that takes an array of numbers as parameter
+            //  and returns an array where the elements are sorted in ascending numerical order
+            //  When you are done, add a second boolean parameter to the function
+            //  If it is `true` sort the array descending, otherwise ascending
+
+
+            //  Example:
+            //Console.WriteLine(Bubble(new int[] { 34, 12, 24, 9, 5 }));
+            //  should print [5, 9, 12, 24, 34]
+
+            var firstFunc = Bubble(new int[] { 34, 12, 24, 9, 5 });
+            foreach (var item in firstFunc)
+            {
+                Console.Write(item + " ");
+            };
+
+
+            Console.WriteLine();
+
+
+            //Console.WriteLine(AdvancedBubble(new int[] { 34, 12, 24, 9, 5 }, true));
+            //  should print [34, 24, 12, 9, 5]
+
+            var secFunc = AdvancedBubble(new int[] { 34, 12, 24, 9, 5 }, true);
+            foreach (var item in secFunc)
+            {
+                Console.Write(item + " ");
+            };
+        }
+
+        public static Array Bubble(int[] userInput)
+        {
+            Array.Sort(userInput);
+
+            return userInput;
+        }
+
+
+        public static Array AdvancedBubble(int[] userInput, bool userComment)
+        {
+            Array.Sort(userInput);
+            if (userComment)
+            {
+                Array.Reverse(userInput);
+                return userInput;
+            }
+
+            return userInput;
+        }
+
+    }
+}
+
---
+++ Functions/PalindromeSearcher/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PalindromeSearcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create a function named searchPalindrome() following your current language's style guide.
+            //It should take a string, search for palindromes that is at least 3 characters long and return a list with the found palindromes.
+
+            //           input                                             output
+
+            //   "dog goat dad duck doodle never"           ["og go", "g g", " dad ", "dad", "d d", "dood", "eve"]
+            //   "apple"                                    []
+            //   "racecar"                                  ["racecar", "aceca", "cec"]
+            //   ""                                         []
+
+            //Console.WriteLine(searchPalindrome("dog goat dad duck doodle never"));
+            Console.WriteLine(searchPalindrome("dog goat"));
+
+
+        }
+
+
+        public static string searchPalindrome(string userInput)
+        {
+            //var result = new List<string> { };
+            var result = "";
+            int indexCounter = 2;
+            int subStart = 0;
+
+            while (indexCounter < userInput.Length)
+            {
+
+
+                string checkWord = userInput.Substring(subStart, indexCounter);
+
+
+
+                for (int start = 2; start < userInput.Length; start++)
+                {
+                    char[] toArray = checkWord.ToCharArray();
+                    Array.Reverse(toArray);
+                    string resultReversed = String.Join(' ', toArray);
+
+                    if (checkWord == resultReversed)
+                    {
+                        //result.Add($"{checkWord} {resultReversed}");
+                        result += checkWord + " " + resultReversed + ",";
+                    }
+                }
+
+                indexCounter++;
+                subStart++;
+            }
+            //1. make a loop
+            //take the first 3 index
+            //2. check if it's palindrome, push to empty array
+            // 3. add one more char to the loop and check the word again
+            // 4. in each loop it must move one index forthure for start of Substering( ) method
+
+            return result;
+        }
+    }
+}
+
