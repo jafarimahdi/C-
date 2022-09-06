62e63eaa35dd3390fdc34fe5f8a197c8725352df
jafarimahdi <mahdijafarii.mj@gmail.com>
2022-09-06 10:13:31 +0200

    .Net Collection - String

---
+++ NET Collections/Reverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Reverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string toBeReversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+            // Create a method that can reverse a string which is passed as q parameter
+            // Pass the `toBeReversed` variable to this method to check if it works well
+            // At first, solve this task using the `String.Chars[Int32]` property, e.g. `toBeReversed[0]`
+            // Try other solutions when you are done
+
+            Console.WriteLine(Reverse(toBeReversed));
+        }
+
+        public static string Reverse(string userInput)
+        {
+            char[] convert = userInput.ToCharArray();
+            Array.Reverse(convert);
+
+            string result = String.Join("", convert);
+
+            return result;
+        }
+    }
+}
+
---
+++ NET Collections/TodoPrint/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace TodoPrint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string todoText = " - Buy milk\n";
+            // Add "My todo:" to the beginning of the `todoText`
+            // Add " - Download games" to the end of the `todoText`
+            // Add " - Diablo" to the end of the `todoText` applying indentation
+
+            // Expected output:
+
+            // My todo:
+            //  - Buy milk
+            //  - Download games
+            //      - Diablo
+
+            //StringBuilder convertToDo = new StringBuilder(todoText);
+
+            string result = todoText.Insert(0, "My todo:\n");
+            result += " - Download games\n";
+            result += "     - Diablo ";
+
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
+
