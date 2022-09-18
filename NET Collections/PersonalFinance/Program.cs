85ac6b02aab8cd1e5e0d7940992e8a832dd61058
jafarimahdi <mahdijafarii.mj@gmail.com>
2022-09-18 15:27:19 +0200

    Personal finance

--- NET Collections/PersonalFinance/Program.cs
+++ NET Collections/PersonalFinance/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using static System.Net.Mime.MediaTypeNames;
+using System.Security.Cryptography;
+using System.Collections.Generic;
 
 namespace PersonalFinance
 {
@@ -6,7 +9,22 @@ namespace PersonalFinance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // We are going to represent our expenses in a list containing integers:
+
+            //Create a list with the following items:
+            //500, 1000, 1250, 175, 800 and 120
+            //Create an application which prints out the answers to the following questions:
+            //How much did we spend?
+            //Which was our greatest expense?
+            //Which was our cheapest spending?
+            //What was the average amount of our spendings ? (print this as a float value)
+
+            List<int> Personalfinance = new List<int>(){
+                500, 1000, 1250, 175, 800,120
+            };
+
+
         }
     }
 }
