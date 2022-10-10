147d181b9a7d6531fa04f918dcb439d918f2c5f1
jafarimahdi <mahdijafarii.mj@gmail.com>
2022-10-10 07:49:02 +0200

    weekend Practice

---
+++ WeekendPractice/Net Collection/StudentCounter/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var studentDictionaries = new List<Dictionary<string, object>>();
+
+            // Given this list of dictionaries...
+
+            var student0 = new Dictionary<string, object>();
+            student0.Add("name", "Theodor");
+            student0.Add("age", 9);
+            student0.Add("candies", 2);
+            studentDictionaries.Add(student0);
+
+            var student1 = new Dictionary<string, object>();
+            student1.Add("name", "Paul");
+            student1.Add("age", 10);
+            student1.Add("candies", 1);
+            studentDictionaries.Add(student1);
+
+            var student2 = new Dictionary<string, object>();
+            student2.Add("name", "Mark");
+            student2.Add("age", 7);
+            student2.Add("candies", 3);
+            studentDictionaries.Add(student2);
+
+            var student3 = new Dictionary<string, object>();
+            student3.Add("name", "Peter");
+            student3.Add("age", 12);
+            student3.Add("candies", 5);
+            studentDictionaries.Add(student3);
+
+            var student4 = new Dictionary<string, object>();
+            student4.Add("name", "Olaf");
+            student4.Add("age", 12);
+            student4.Add("candies", 7);
+            studentDictionaries.Add(student4);
+
+            var student5 = new Dictionary<string, object>();
+            student5.Add("name", "George");
+            student5.Add("age", 3);
+            student5.Add("candies", 2);
+            studentDictionaries.Add(student5);
+
+            // Create a function called `ListOfNames()` which takes a list of student dictionaries and returns
+            // the names of students (as a list of strings) who have more than 4 candies
+
+            // Expected output: "Peter", "Olaf"
+            ListOfNames(studentDictionaries);
+
+            // Create a function called `SumOfAges()` which takes a list of student dictionaries and returns
+            // the sum (as an integer) of the ages of students who have less than 5 candies
+
+            // Expected output: 29
+            Console.WriteLine(SumOfAges(studentDictionaries));
+        }
+        // end of the main
+
+        public static List<string> ListOfNames(List<Dictionary<string, object>> userInput)
+        {
+            List<string> namesWithMoreCandies = new List<string>() { };
+
+            foreach (var item in userInput)
+            {
+                int candies = userInput.GetValueOrDefault(item)
+                if (candies > 4)
+                {
+                    namesWithMoreCandies.Add(item["name"]);
+                }
+
+            }
+
+            foreach (var item in namesWithMoreCandies)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+    }
+}
+
