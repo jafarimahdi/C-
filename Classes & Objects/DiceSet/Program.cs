ffcdfb680bd1295d142b0fc776b2b15ad241920e
jafarimahdi <mahdijafarii.mj@gmail.com>
2022-10-12 08:40:27 +0200

    class & object: diceSet

---
+++ Classes & Objects/DiceSet/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DiceSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // You have a `DiceSet` class which has a list for 6 dice
+            // You can roll all of them with roll()
+            // Check the current rolled numbers with getCurrent()
+            // You can reroll with reroll()
+            // Your task is to roll the dice until all of the dice are 6
+
+
+            DiceSet diceSet = new DiceSet();
+
+            Console.WriteLine(diceSet.GetCurrent());
+            diceSet.Roll();
+            Console.WriteLine(diceSet.GetCurrent());
+            Console.WriteLine(diceSet.GetCurrent(5));
+            diceSet.Reroll();
+            Console.WriteLine(diceSet.GetCurrent());
+            diceSet.Reroll(4);
+            Console.WriteLine(diceSet.GetCurrent());
+        }
+    }
+}
+
