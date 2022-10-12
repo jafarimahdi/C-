3e380e080c4ad3a231a63bc4aad5f37a5b6ffd3a
jafarimahdi <mahdijafarii.mj@gmail.com>
2022-10-12 08:40:02 +0200

    Class & object: Counter

---
+++ Classes & Objects/Counter/Counter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Counter
+{
+    public class Counter
+    {
+        int counter = 0;
+
+        public Counter(int _counter)
+        {
+            counter = _counter;
+        }
+
+        public int Add()
+        {
+            counter++;
+            return counter;
+
+        }
+        public int Add(int userNumber)
+        {
+            counter += userNumber;
+            return counter;
+        }
+        public int Get()
+        {
+            return counter;
+        }
+        public void Reset()
+        {
+            counter = 0;
+        }
+    }
+}
+
---
+++ Classes & Objects/Counter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Reflection;
+using System.Reflection.Metadata;
+
+namespace Counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Counter class
+            // which has a counter(integer) field
+            // at creation it should have a default value 0
+            // there are two ways to create the class:
+            // with 0 input parameters
+            // with 1 input parameter called counter which is set as the initial value of the counter field
+            // we can add another whole number to this counter: Add(number)
+            // we can just increase the counter's value by one: Add() (no parameters)
+            // we can Get() the current counter value
+            // and we can Reset() the counter to the initial value set during the construction of the class
+
+            Counter firstCounter = new Counter(1);
+            firstCounter.Add(12);
+            Console.WriteLine(firstCounter.Get());
+
+            firstCounter.Reset();
+            Console.WriteLine(firstCounter.Get());
+
+
+        }
+    }
+}
+
