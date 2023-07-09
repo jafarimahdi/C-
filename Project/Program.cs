97b5245fa935d7edfb07239ed091159824842873
Mate <mahdijafarii.mj@gmail.com>
2023-07-09 12:18:44 +0200

    Phone Ai Tech Advisor application

---
+++ Project/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CarClassLibrary;
+
+namespace CarShopConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            Console.WriteLine("Car c is as follows:" + c.Make + "" + c.Model + "" + c.Price);
+
+            Console.ReadLine();
+        }
+    }
+}
