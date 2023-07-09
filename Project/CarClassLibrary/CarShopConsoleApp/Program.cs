81a373cb5fe38fb5bc1952c83f9e4acb8e28b2fa
Mate <mahdijafarii.mj@gmail.com>
2023-07-09 12:17:35 +0200

    Car Application

---
+++ Project/CarClassLibrary/CarShopConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using CarClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarShopConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+            Console.WriteLine("Car c is as follows" + c.Make + " "+c.Model + " " + c.Price  );
+            Console.ReadLine();
+
+        }
+    }
+}
